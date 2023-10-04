d9aa321417e7e6acd6aa1498383cbf06974ac635
Genesis Barrios <genesisbarriosdev@gmail.com>
2023-10-03 21:09:24 -0400

    update directorio getUsers call from 20 to10 users

--- pages/api/getAllUsers.ts
+++ pages/api/getAllUsers.ts
@@ -12,7 +12,7 @@ interface ResponseData {
 }
 const getAllUsers = async (page:number) =>{
     await dbConnect();
-    const Users = await users.find().skip(page*5).limit(20);
+    const Users = await users.find().skip(page*5).limit(10);
     return Users;
 }
 
