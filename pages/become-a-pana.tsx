7efae813e8d66a67f85fa94d6f6548efcf7baea1
legendary-jld <jeremy.l.downs@gmail.com>
2023-10-20 22:50:14 -0400

    Become A Pana and footer minor updates

--- pages/become-a-pana.tsx
+++ pages/become-a-pana.tsx
@@ -12,7 +12,7 @@ const BecomeAPana: NextPage = () => {
         desc="Sign up to become a Pana and get the benefits of being liste on our directory!"
         />
       <div className={styles.main}>
-        <iframe className={styles.fullFrame} src="https://docs.google.com/forms/d/e/1FAIpQLSdE7qckjuydnNl4GPLyyU6whh89MuOGTIEZIaI5EhFfOk4wVA/viewform"></iframe>
+        <iframe className={styles.fullFrame} src="https://docs.google.com/forms/d/e/1FAIpQLSdE7qckjuydnNl4GPLyyU6whh89MuOGTIEZIaI5EhFfOk4wVA/viewform?embedded=true">Loading...</iframe>
       </div>
   </main>
   )
