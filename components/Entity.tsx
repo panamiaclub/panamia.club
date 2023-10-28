5c16bba27da43852dd457a7ef392f61bbed588cb
legendary-jld <jeremy.l.downs@gmail.com>
2023-10-27 20:51:05 -0400

    New home page images

---
+++ components/Entity.tsx
@@ -0,0 +1,12 @@
+import styles from './Entity.module.css';
+
+interface EntityProps {
+    text: string,
+}
+
+export default function Entity(props: EntityProps) {
+    return (
+        <span className={styles.entity}>{props.text}</span>
+    );
+
+}
