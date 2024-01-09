0ddc91ab1997b8ab59fec757a00384940de1df1a
Genesis Barrios <genesisbarriosdev@gmail.com>
2024-01-09 01:56:55 -0500

    added backend api route for import profiles

--- pages/account/admin/import.tsx
+++ pages/account/admin/import.tsx
@@ -1,9 +1,38 @@
 import React, { useState } from "react";
 import * as XLSX from "xlsx";
+import axios from "axios";
 
 function ExcelToJsonConverter() {
   const [file, setFile] = useState(null);
+  const [importConfirmation, setImportConfirmation] = useState(false);
   const [jsonData, setJsonData] = useState("");
+
+  async function importUsers() {
+        const res = await axios
+        .post(
+            "/api/importProfiles",
+            { jsonData },//add slug
+            {
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                },
+            }
+        )
+        .then(async (response) => {
+            if (response.data.error) {
+                alert(response.data.error) // soft error should display for user to correct
+            } else {
+                setImportConfirmation(true);
+                alert("Profiles have been imported!");
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+            alert("There was a problem uploading these users: " + error.message);
+        });
+        console.log(res);
+}
 
   const handleConvert = () => {
     if (file) {
@@ -15,6 +44,7 @@ function ExcelToJsonConverter() {
         const worksheet = workbook.Sheets[sheetName];
         const json = XLSX.utils.sheet_to_json(worksheet);
         setJsonData(JSON.stringify(json, null, 2));
+        console.log(json)
       };
       reader.readAsBinaryString(file);
     }
