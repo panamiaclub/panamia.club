c7890fc517e53070b0032b9095347d851339d9ec
Jeremy Downs <jeremy.l.downs@gmail.com>
2024-02-07 20:29:09 -0500

    New About us updates

--- pages/about-us2.tsx
+++ pages/about-us2.tsx
@@ -1,12 +1,63 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
+import { useState } from 'react';
+import classNames from 'classnames';
 import { IconCash } from '@tabler/icons';
-
-import PanaLogo from '@/components/PanaLogo';
-import styles from '@/styles/AboutUs2.module.css'
 import Link from 'next/link';
+
+import styles from '@/styles/AboutUs2.module.css'
 import PanaButton from '@/components/PanaButton';
 
+const ProjectsBlock = () => {
+
+  const [project, setProject] = useState("directorio");
+
+  return (
+    <div className={classNames(styles.projectsBlock, project)}>
+      <div className={styles.projectsHeader}>
+        <h3 
+        className={project == "directorio" ? styles.directorioTitleActive : styles.directorioTitle}
+        onClick={ (e:any) => {setProject('directorio')}}
+        >El Directorio</h3>
+        <h3 
+        className={project == "leolero" ? styles.leoleroTitleActive : styles.leoleroTitle}
+        onClick={ (e:any) => {setProject('leolero')}}
+        >LeoLero</h3>
+        <h3 
+        className={project == "panavizion" ? styles.panavizionTitleActive : styles.panavizionTitle}
+        onClick={ (e:any) => {setProject('panavizion')}}
+        >PanaVizion</h3>
+      </div>
+      { project == "directorio" && 
+        <p className={styles.directorioBody}>El Directorio is your access guide to everything locally made and occurring in SoFlo! 
+        Our online Local Directory, with easy keyword search functionality, allows patrons to 
+        find locally sourced solutions for any need or desire. This enhances convenience for 
+        consumers when shopping locally and provides increased visibility for local brands, 
+        service providers, and organizations. Our objective is to create a tool that will 
+        stimulate the local SoFlo economy and advocate for a lifestyle centered around supporting 
+        local businesses.</p>
+      }
+      { project == "leolero" && 
+        <p className={styles.leoleroBody}>Discover the vibrant world of LeoLero, our monthly newsletter that brings you a curated 
+        selection of exclusive insights. Dive into excerpts from our talented Panas, catch recaps 
+        from our podcast, and join engaging conversations led by seasoned experts. Explore topics 
+        that resonate with the SoFlo community and stay in the loop with a thoughtfully crafted 
+        playlist and a local events calendar for South Florida. LeoLero is your key to the latest 
+        happenings, diverse voices, and the heartbeat of our creative community.</p>
+      }
+      { project == "panavizion"  && 
+        <p className={styles.panavizionBody}>PanaVizión serves as a broadcast channel and podcast dedicated to highlighting impactful 
+        community leaders in South Florida through curated media content that shares their stories. 
+        Our goal is to uncover the rich experiences, talents, and creations that the people of 
+        South Florida have to offer. By providing a platform for these stories, we strive to expand 
+        the public's perspective and appreciation for the diverse narratives within the community.</p>
+      }
+      
+    </div>
+  );
+}
+
 const AboutUs: NextPage = () => {
+  
   // Hero Image & Vision
   // Mission Statement
   // VIDEO
@@ -51,30 +102,10 @@ const AboutUs: NextPage = () => {
             </div>
           </section>
           <section className={styles.sectionProjects}>
-            <h2>Our Projects</h2>
-            <h3>El Directorio</h3>
-            <p>El Directorio is your access guide to everything locally made and occurring in SoFlo! 
-              Our online Local Directory, with easy keyword search functionality, allows patrons to 
-              find locally sourced solutions for any need or desire. This enhances convenience for 
-              consumers when shopping locally and provides increased visibility for local brands, 
-              service providers, and organizations. Our objective is to create a tool that will 
-              stimulate the local SoFlo economy and advocate for a lifestyle centered around supporting 
-              local businesses.</p>
-
-            <h3>LeoLero</h3>
-            <p>Discover the vibrant world of LeoLero, our monthly newsletter that brings you a curated 
-              selection of exclusive insights. Dive into excerpts from our talented Panas, catch recaps 
-              from our podcast, and join engaging conversations led by seasoned experts. Explore topics 
-              that resonate with the SoFlo community and stay in the loop with a thoughtfully crafted 
-              playlist and a local events calendar for South Florida. LeoLero is your key to the latest 
-              happenings, diverse voices, and the heartbeat of our creative community.</p>
-
-            <h3>PanaVizión</h3>
-            <p>PanaVizión serves as a broadcast channel and podcast dedicated to highlighting impactful 
-              community leaders in South Florida through curated media content that shares their stories. 
-              Our goal is to uncover the rich experiences, talents, and creations that the people of 
-              South Florida have to offer. By providing a platform for these stories, we strive to expand 
-              the public's perspective and appreciation for the diverse narratives within the community.</p>
+            <div className={styles.containerProjects}>
+              <h2>Our Projects</h2>
+              <ProjectsBlock />
+            </div>
           </section>
 
           <section className={styles.sectionBoard}>
@@ -82,7 +113,7 @@ const AboutUs: NextPage = () => {
               <h2>Our Board</h2>
               <div className={styles.boardBlock}>
                 <div className={styles.boardImg}>
-                  <img src="/img/about/anettemago.png" />
+                  <img src="/img/about/anette_mago.jpg" />
                 </div>
                 <div>
                   <h3>Anette Mago, Co-Founder</h3>
@@ -98,7 +129,7 @@ const AboutUs: NextPage = () => {
               
               <div className={styles.boardBlock}>
                 <div className={styles.boardImg}>
-                <img src="/img/about/claribelavila.png" />
+                <img src="/img/about/claribel_avila.jpg" />
                 </div>
                 <div>
                   <h3>Claribel Avila, Co-Founder</h3>
@@ -119,48 +150,58 @@ const AboutUs: NextPage = () => {
               <h2>Our Team</h2>
               <div className={styles.teamBlocks}>
                 <div className={styles.teamPicture}>
-                  <img src="/img/about/beemaria.png" />
+                  <img src="/img/about/bee_maria.jpg" />
                   <h3>Bee Maria<br /><small>Copywriter</small></h3>
                 </div>
                 <div className={styles.teamPicture}>
-                  <img src="/img/about/jeremydowns.png" />
+                  <img src="/img/about/jeremy_downs.jpg" />
                   <h3>Jeremy Downs<br /><small>Technical Advisor</small></h3>
                 </div>
                 <div className={styles.teamPicture}>
-                  <img src="/img/about/genbarrios.png" />
+                  <img src="/img/about/gen_barrios.jpg" />
                   <h3>Genesis Barrios<br /><small>Web Developer</small></h3>
                 </div>
                 <div className={styles.teamPicture} hidden>
-                  <img src="/img/about/beemaria.png" />
+                  <img src="/img/about/bee_maria.jpg" />
                   <h3>Bryan Torres<br /><small>Branding Director</small></h3>
                 </div>
                 <div className={styles.teamPicture} hidden>
-                  <img src="/img/about/beemaria.png" />
+                  <img src="/img/about/bee_maria.jpg" />
                   <h3>Otto Munos<br /><small>Head Event Coordinator</small></h3>
                 </div>
                 <div className={styles.teamPicture} hidden>
-                  <img src="/img/about/beemaria.png" />
+                  <img src="/img/about/bee_maria.jpg" />
                   <h3>Jose Sifuentes<br /><small>Branding</small></h3>
                 </div>
               </div>
             </div>
           </section>
+          <section className={styles.sectionMap} hidden>
+            <div className={styles.mapImg}>
+              <img src="/img/about/florida_panamia2.jpg" />
+            </div>
+          </section>
           <section className={styles.sectionSupport}>
             <div className={styles.containerSupport}>
-              <h2>Support Our Club</h2>
-              <p>Pana MIA Club works hard towards our vision for a unified local SoFlo community 
-                everyday. We know we can do it with your help! You can support us by funding our 
-                mission with a one-time donation or by joining our community of supporters called 
-                Gente dePana!</p>
-              <p>
-                <PanaButton color="pink" href="/donate/">
-                Help Fund Our Open-Access Local's Directory
-                </PanaButton>
-              </p>
-              <p>Our Gente dePana subscribers are the foundation of Pana MIA's sustainability, monthly 
-                contributions allow us to make bigger strides in our projects to support the local 
-                community. In return, our Gente are rewarded with so many benefits, discounts and perks 
-                that give you special access to all things Pana!</p>
+              <div className={styles.mapImg}>
+                <img src="/img/about/florida_panamia2.jpg" />
+              </div>
+              <div>
+                <h2>Support Our Club</h2>
+                <p>Pana MIA Club works hard towards our vision for a unified local SoFlo community 
+                  everyday. We know we can do it with your help! You can support us by funding our 
+                  mission with a one-time donation or by joining our community of supporters called 
+                  Gente dePana!</p>
+                <p>
+                  <PanaButton color="pink" href="/donate/">
+                  Help Fund Our<br />Open-Access Local's Directory
+                  </PanaButton>
+                </p>
+                <p>Our Gente dePana subscribers are the foundation of Pana MIA's sustainability, monthly 
+                  contributions allow us to make bigger strides in our projects to support the local 
+                  community. In return, our Gente are rewarded with so many benefits, discounts and perks 
+                  that give you special access to all things Pana!</p>
+              </div>
             </div>
           </section>
           <div className={styles.footer}>
