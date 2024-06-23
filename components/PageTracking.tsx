bd8cece4e3df0b8a2e69a15fe5ae6f81c357906a
Jeremy Downs <jeremy.l.downs@gmail.com>
2024-06-23 14:01:58 -0400

    Add Gtag for Analytics

--- components/PageTracking.tsx
+++ components/PageTracking.tsx
@@ -8,11 +8,21 @@ export default function PageTracking() {
     })(window,document,'script','dataLayer','GTM-TVN3XKX2');</script>\
     <!-- End Google Tag Manager -->`
 
+    const trackingGTAGAnalytics = `<!-- Google tag (gtag.js) -->\
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-H9HZTY30DN"></script>\
+    <script>\
+    window.dataLayer = window.dataLayer || [];\
+    function gtag(){dataLayer.push(arguments);}\
+    gtag('js', new Date());\
+    gtag('config', 'G-H9HZTY30DN');\
+    </script>`
+
     const trackingMetricool = "<script>function loadScript(a){var b=document.getElementsByTagName('head')[0],c=document.createElement('script');c.type='text/javascript',c.src='https://tracker.metricool.com/resources/be.js',c.onreadystatechange=a,c.onload=a,b.appendChild(c)}loadScript(function(){beTracker.t({hash:'22b37296cef855a47d27540f9aadd51'})});</script>";
 
     return (
         <>
         <div dangerouslySetInnerHTML={ {__html: trackingGTAG} }></div>
+        <div dangerouslySetInnerHTML={ {__html: trackingGTAGAnalytics} }></div>
         <div dangerouslySetInnerHTML={ {__html: trackingMetricool} }></div>
         </>
     );
